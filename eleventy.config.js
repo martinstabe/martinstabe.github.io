afd539c0b1a64639371b8e7dd15e645cc4be8faa
Martin Stabe <martinstabe@gmail.com>
2026-04-06 21:39:45 +0300

    Implement Phase 2

--- eleventy.config.js
+++ eleventy.config.js
@@ -6,6 +6,28 @@ module.exports = function (eleventyConfig) {
     "googlea1fed2510060dd96.html": "googlea1fed2510060dd96.html"
   });
 
+  eleventyConfig.addFilter("displayDate", (value) => {
+    if (!value) {
+      return "";
+    }
+
+    const date = value instanceof Date ? value : new Date(value);
+    if (Number.isNaN(date.getTime())) {
+      return "";
+    }
+
+    return new Intl.DateTimeFormat("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      timeZone: "Europe/London"
+    }).format(date);
+  });
+
+  eleventyConfig.addShortcode("currentYear", () => {
+    return new Date().getFullYear();
+  });
+
   return {
     dir: {
       input: "src",
